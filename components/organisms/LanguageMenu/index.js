15b34c44bcad3ed1ad1321156613337c0f38fd4c
tlmn <mail@tlmn.io>
2022-08-16 16:25:53 +0200

    close language menu on click

--- components/organisms/LanguageMenu/index.js
+++ components/organisms/LanguageMenu/index.js
@@ -1,8 +1,10 @@
 import useTranslation from "next-translate/useTranslation";
 import setLanguage from "next-translate/setLanguage";
 import clsx from "clsx";
+import useAppContext from "src/lib/useAppContext";
 
 const LanguageMenu = ({ className = "" }) => {
+  const { setAppState } = useAppContext();
   const LANGUAGES = ["de", "en", "fr", "es", "vi", "tr", "bn", "ar", "fa"];
   const { t } = useTranslation();
 
@@ -10,7 +12,12 @@ const LanguageMenu = ({ className = "" }) => {
     <ul className={clsx(className, "flex flex-col gap-xs items-end")}>
       {LANGUAGES.map((language, index) => (
         <li key={index} className="menuButton w-max right-0">
-          <button onClick={async () => await setLanguage(language)}>
+          <button
+            onClick={async () => {
+              setAppState((prev) => ({ ...prev, showLanguageMenu: false }));
+              await setLanguage(language);
+            }}
+          >
             {t(`menu:${language}`)}
           </button>
         </li>
