c759683e299c62bd7fb3f52a9c235056fd1e6d9c
tlmn <mail@tlmn.io>
2022-08-18 12:09:37 +0200

    remove turkish

--- i18n.js
+++ i18n.js
@@ -1,5 +1,5 @@
 module.exports = {
-  locales: ["de", "en", "fr", "es", "vi", "tr", "bn", "ar", "fa"],
+  locales: ["de", "en", "fr", "es", "vi", "bn", "ar", "fa"],
   defaultLocale: "de",
   pages: {
     "/": [
