78cdfc16524f0bc12cada2688ab70940db85cbca
tlmn <mail@tlmn.io>
2022-08-25 11:06:44 +0200

    add closing icon with circle

---
+++ src/assets/svgs/closing--circle.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ClosingIconCircle = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 112 112"
+  >
+    <circle
+      cx="54.485"
+      cy="54.485"
+      r="49.944"
+      fill="red"
+      stroke="#F8CAFF"
+      strokeWidth="9.081"
+    ></circle>
+    <path
+      fill="#F8CAFF"
+      fillRule="evenodd"
+      d="M71.524 76.734l4.938-4.97L60.57 55.77 77 39.236l-5.07-5.103L55.5 50.668 38.939 34 34 38.97l16.562 16.668-16.156 16.26L39.476 77l16.156-16.26 15.892 15.994z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+);
+
+export default ClosingIconCircle;
